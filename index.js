07e116f4b43b9f54bec9e44590532da3a8137f7d
Shreyas Hupare <shreyas@SMH-MacBook-Pro.local>
2022-08-26 13:09:58 +0530

    delete, find by id, put implemented without route, controllers

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ sequelize.authenticate().then(()=>{
 
 //create table
 const Employee = sequelize.define('Employee',{
-    Emp_id:Sequelize.INTEGER,
+    Emp_id:{
+        type:Sequelize.INTEGER,
+        primaryKey: true,
+    },
     name:Sequelize.STRING,
     age:Sequelize.INTEGER,
 });
@@ -29,12 +32,32 @@ const Employee = sequelize.define('Employee',{
 sequelize.sync();
 
 //findAll
-app.get('/',async(req, res) =>{
-    const findAll = await Employee.findAll();
-    res.send(findAll);
+app.get('/findAll',async(req, res) =>{
+    try{
+        const findAll = await Employee.findAll();
+        res.send(findAll);
+    }
+    catch(err)
+    {
+        res.send('Error : '+err);
+    }    
+});
+//find by id
+app.get('/findbyId/:id', async(req, res)=>{
+    try{
+        const user = await Employee.findOne({
+            where:{
+                Emp_id: req.params.id,
+            }
+        });
+        res.send(user);
+    }
+    catch(err){
+        res.send('Error: '+err);
+    }
 });
 
-app.post('/',async(req, res)=>{
+app.post('/add',async(req, res)=>{
     const Emp_id=req.body.Emp_id;
     const name=req.body.name;
     const age=req.body.age;
@@ -43,10 +66,54 @@ app.post('/',async(req, res)=>{
         name,
         age,
     });
-    await insertEmp.save();
-    res.send('Employee added successfully');
-    console.log("Employee added successfully");
+    try{
+        await insertEmp.save();
+        res.statusCode = 200;
+        res.send('Employee added successfully');
+    }catch(err){
+        res.send('Employee Not added. Error : '+ err);
+    }
+    // console.log("Employee added successfully");
 })
+
+//update data
+
+app.put('/update',(req, res)=>{
+    const Emp_id=req.body.Emp_id;
+    const name=req.body.name;
+    const age=req.body.age;
+    try{
+        Employee.update(
+            {
+                name: name,
+                age: age,
+            },
+            { where:{
+                Emp_id: Emp_id,
+            }
+            }
+        );
+        res.send('Employee details updated successfully');
+    }
+    catch(err){
+        res.send('Error : '+err);
+    }
+});
+//delete by id
+app.delete('/delete/:id', (req, res)=>{
+    const id = req.params.id;
+    try{
+        Employee.destroy({
+            where :{
+                Emp_id: id,
+            },
+        });
+        res.send(`Employee with id ${id} deleted successfully`);
+    }
+    catch(err){
+        res.send('Error : '+err);
+    }
+});
 
 app.listen(PORT, () =>{
     console.log(`Server started at port ${PORT}`)
